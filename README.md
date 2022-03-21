c618fbbec11132f3e982506221f807934207773d
Reece McMillin <reecemcmillin@gmail.com>
2022-03-21 12:00:02 -0500

    auto-formatted SQL for consistency, added IF NOT EXISTS to CREATEs, wrote detailed Docker setup instructions in README.md

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # database
 Database layout for test data on students, faculty, courses, etc.
+
+## Docker Setup
+
+### Container
+1. In your shell, build and run a new Docker container named `umkcdb`.
+   - `-d` runs the container in the background (detached).
+   - `-p 3306:3306` just maps `localhost:3306` to the same port on the container.
+2. MySQL will generate a random password stored in the Docker logs, it can be pulled out with `grep`.
+3. Copy the password and use it to authenticate with the third command, which starts a MySQL session in the Docker container.
+
+```console
+$ docker run -p 3306:3306 --name=umkcdb -d mysql/mysql-server:latest
+$ docker logs 2>&1 umkcdb | grep GENERATED
+$ docker exec -it umkcdb mysql -uroot -p
+```
+
+I ran into a problem with a different MySQL instance already listening on port 3306, so I found its process ID (969, yours will be different) and stopped it with these commands.
+
+```console
+$ sudo netstat -pna | grep 3306
+tcp        0        0 0.0.0.0:3306        0.0.0.0:*        LISTEN 969/mysql
+$ sudo kill 969
+```
+
+### MySQL
+
+1. Change the root user's password to something more memorable, for local testing I just use `root` but we'll use something more secure when we actually build.
+2. MySQL is set to only allow communication from inside the image by default, so update the `host` to `%` to open up communication to the outside world.
+
+```sql
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'newPassword';
+UPDATE mysql.user SET host = '%' WHERE user='root';
+```
+
+Now we can quit the container (Ctrl+D first out of MySQL and again out of the container) and restart it.
+
+```console
+$ docker restart umkcdb
+```
+
+Now we should be able to connect to the MySQL server from outside of the container from something like MySQL Workbench.
--- UMKC DB.sql
+++ UMKC DB.sql
@@ -1,46 +1,45 @@
-CREATE DATABASE UMKC_Db
-
-USE UMKC_Db
-
-CREATE TABLE Student
-(Student_ID INT NOT NULL,
-Student_FName VARCHAR (20) NOT NULL,
-Student_LName VARCHAR (20) NOT NULL,
-Student_Email VARCHAR(30) NOT NULL,
-Student_InternationalStatus BOOLEAN,
-Student_GTACert BOOLEAN,
-Student_Qualifications VARCHAR(10),
-Student_Major VARCHAR(20),
-Student_NumOfSem INT NOT NULL,
-PRIMARY KEY (Student_ID));
-
-CREATE TABLE Course
-(Course_ID INT NOT NULL,
-Course_Name VARCHAR(20),
-PRIMARY KEY (Course_ID));
-
-CREATE TABLE Grade
-(Student_ID INT NOT NULL,
-Course_ID INT NOT NULL,
-Grade_Value VARCHAR(1),
-PRIMARY KEY (Student_ID, Course_ID),
-FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT);
-
-CREATE TABLE Faculty
-(Faculty_ID INT NOT NULL,
-Faculty_FName VARCHAR(20) NOT NULL,
-Faculty_LName VARCHAR(20) NOT NULL,
-Faculty_Email VARCHAR(30) NOT NULL,
-PRIMARY KEY (Faculty_ID));
-
-CREATE TABLE Positions
-(Position_ID INT NOT NULL,
-Student_ID INT NOT NULL,
-Faculty_ID INT NOT NULL,
-Course_ID INT NOT NULL,
-Position_Type VARCHAR(10) NOT NULL,
-PRIMARY KEY (Position_ID),
-FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Faculty_ID) references Faculty (Faculty_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT);
+CREATE DATABASE IF NOT EXISTS UMKC_Db;
+USE UMKC_Db;
+CREATE TABLE IF NOT EXISTS Student (
+    Student_ID INT NOT NULL,
+    Student_FName VARCHAR (20) NOT NULL,
+    Student_LName VARCHAR (20) NOT NULL,
+    Student_Email VARCHAR(30) NOT NULL,
+    Student_InternationalStatus BOOLEAN,
+    Student_GTACert BOOLEAN,
+    Student_Qualifications VARCHAR(10),
+    Student_Major VARCHAR(20),
+    Student_NumOfSem INT NOT NULL,
+    PRIMARY KEY (Student_ID)
+);
+CREATE TABLE IF NOT EXISTS Course (
+    Course_ID INT NOT NULL,
+    Course_Name VARCHAR(20),
+    PRIMARY KEY (Course_ID)
+);
+CREATE TABLE IF NOT EXISTS Grade (
+    Student_ID INT NOT NULL,
+    Course_ID INT NOT NULL,
+    Grade_Value VARCHAR(1),
+    PRIMARY KEY (Student_ID, Course_ID),
+    FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
+    FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT
+);
+CREATE TABLE IF NOT EXISTS Faculty (
+    Faculty_ID INT NOT NULL,
+    Faculty_FName VARCHAR(20) NOT NULL,
+    Faculty_LName VARCHAR(20) NOT NULL,
+    Faculty_Email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Faculty_ID)
+);
+CREATE TABLE IF NOT EXISTS Positions (
+    Position_ID INT NOT NULL,
+    Student_ID INT NOT NULL,
+    Faculty_ID INT NOT NULL,
+    Course_ID INT NOT NULL,
+    Position_Type VARCHAR(10) NOT NULL,
+    PRIMARY KEY (Position_ID),
+    FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
+    FOREIGN KEY (Faculty_ID) references Faculty (Faculty_ID) ON DELETE RESTRICT,
+    FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT
+);
