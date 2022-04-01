99f500c68dfd61983d6d174eea5ce16aff662e64
Thomas Yang <thomasy1275@gmail.com>
2022-04-01 01:18:18 -0500

    Update Generate Fake Data.py
    Added a function to generate fake courses

--- Generate Fake Data.py
+++ Generate Fake Data.py
@@ -53,7 +53,11 @@ VALUES"""
         stu_name = stu_fname + " " + stu_lname
         stu_email = stu_fname.lower() + stu_lname.lower() + "@umsystem.edu"
 
-        fake_stu = fake_stu + stu_id + "'," + "'" + stu_fname + "'," + "'" + stu_lname + "'," + "'" + stu_email + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + random.choice(stu_qualifications) + "'," + "'" + random.choice(stu_major) + "'," + "'" + str(random.randint(1,12)) + "')\n"
+        if i != n:
+            fake_stu = fake_stu + stu_id + "'," + "'" + stu_fname + "'," + "'" + stu_lname + "'," + "'" + stu_email + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + random.choice(stu_qualifications) + "'," + "'" + random.choice(stu_major) + "'," + "'" + str(random.randint(1,12)) + "'),\n"
+        else:
+            fake_stu = fake_stu + stu_id + "'," + "'" + stu_fname + "'," + "'" + stu_lname + "'," + "'" + stu_email + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + str(random.randint(0,1)) + "'," + "'" + random.choice(stu_qualifications) + "'," + "'" + random.choice(stu_major) + "'," + "'" + str(random.randint(1,12)) + "');\n"
+        
         if check_for_dupes(stu_id, stu_name) is bool(True):
             sql_query = sql_query + " " + fake_stu
         i += 1
@@ -75,26 +79,79 @@ VALUES"""
         fac_name = fac_fname + " " + fac_lname
         fac_email = fac_fname.lower() + fac_lname.lower() + "@umsystem.edu"
 
-        fake_fac = fake_fac + fac_id + "'," + "'" + fac_fname + "'," + "'" + fac_lname + "'," + "'" + fac_email + "')\n"
+        if i != n:
+            fake_fac = fake_fac + fac_id + "'," + "'" + fac_fname + "'," + "'" + fac_lname + "'," + "'" + fac_email + "'),\n"
+        else:
+            fake_fac = fake_fac + fac_id + "'," + "'" + fac_fname + "'," + "'" + fac_lname + "'," + "'" + fac_email + "');\n"
+
         if check_for_dupes(fac_id, fac_name) is bool(True):
             sql_query = sql_query + " " + fake_fac
         i += 1
     
     return(sql_query)
 
+def generate_fake_course(n):
+    i = 0
+    course_ab = ["CS", "BIO", "ENG", "ACC", "MGT"]
+    course_type = ["Comp-Sci", "Biology", "English", "Accounting", "Management"]
+    sql_query = """INSERT INTO Course (Course_ID , Course_Type)
+VALUES """
+
+    while i <= n:
+        course_id = ""
+        ran_course_typy = random.choice(course_type)
+        fake_course = "('"
+        course_id_table = []
+
+        if ran_course_typy == "Comp-Sci":
+            temp = course_ab[0]
+            course_id = temp + str(random.randint(100, 499))
+        elif ran_course_typy == "Biology":
+            temp = course_ab[1]
+            course_id = temp + str(random.randint(100, 499))
+        elif ran_course_typy == "English":
+            temp = course_ab[2]
+            course_id = temp + str(random.randint(100, 499))
+        elif ran_course_typy == "Accounting":
+            temp = course_ab[3]
+            course_id = temp + str(random.randint(100, 499))
+        else:
+            temp = course_ab[4]
+            course_id = temp + str(random.randint(100, 499))
+
+        if course_id in course_id_table:
+            generate_fake_course(n)
+        else:
+            course_id_table.append(course_id)
+
+        if i != n:
+            fake_course = fake_course + course_id + "'," + "'" + ran_course_typy + "'),\n"
+        else:
+            fake_course = fake_course + course_id + "'," + "'" + ran_course_typy + "');\n"
+        
+        sql_query = sql_query + fake_course
+        i += 1
+    
+    return(sql_query)
+
+
 def main():
     num_of_stu = 10
     num_of_fac = 3
+    num_of_cour = 5
     f1 = open("fakestudent.txt", "w")
     f2 = open("fakefaculty.txt", "w")
+    f3 = open("fakecourse.txt", "w")
 
     f1.write(generate_fake_stu(num_of_stu))
     f2.write(generate_fake_fac(num_of_fac))
+    f3.write(generate_fake_course(num_of_cour))
 
     print("Done")
 
     f1.close()
     f2.close()
+    f3.close()
 
 if __name__ == '__main__':
     main()
