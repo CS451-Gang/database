ac98376a467b7179c1151dab07a295a973359caf
Thomas Yang <thomasy1275@gmail.com>
2022-04-20 12:41:39 -0500

    Updating deletion cases for each table

--- UMKC DB.sql
+++ UMKC DB.sql
@@ -1,8 +1,7 @@
-CREATE DATABASE UMKC_Db
+CREATE DATABASE IF NOT EXISTS UMKC_Db;
+USE UMKC_Db;
 
-USE UMKC_Db
-
-CREATE TABLE Student
+CREATE TABLE IF NOT EXISTS Student
 (Student_ID INT NOT NULL,
 Student_FName VARCHAR (20) NOT NULL,
 Student_LName VARCHAR (20) NOT NULL,
@@ -14,33 +13,34 @@ Student_Major VARCHAR(20),
 Student_NumOfSem INT NOT NULL,
 PRIMARY KEY (Student_ID));
 
-CREATE TABLE Course
+CREATE TABLE IF NOT EXISTS Course
 (Course_ID VARCHAR(10) NOT NULL,
 Course_Type VARCHAR(20),
 PRIMARY KEY (Course_ID));
 
-CREATE TABLE Grade
+CREATE TABLE IF NOT EXISTS Grade
 (Student_ID INT NOT NULL,
-Course_ID INT NOT NULL,
+Course_ID VARCHAR(10) NOT NULL,
 Grade_Value VARCHAR(1),
 PRIMARY KEY (Student_ID, Course_ID),
-FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT);
+FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE CASCADE,
+FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE CASCADE);
 
-CREATE TABLE Faculty
-(Faculty_ID INT NOT NULL,
-Faculty_FName VARCHAR(20) NOT NULL,
-Faculty_LName VARCHAR(20) NOT NULL,
-Faculty_Email VARCHAR(30) NOT NULL,
-PRIMARY KEY (Faculty_ID));
+CREATE TABLE IF NOT EXISTS Faculty (
+    Faculty_ID INT NOT NULL,
+    Faculty_FName VARCHAR(20) NOT NULL,
+    Faculty_LName VARCHAR(20) NOT NULL,
+    Faculty_Email VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Faculty_ID)
+);
 
-CREATE TABLE Positions
+CREATE TABLE IF NOT EXISTS Positions
 (Position_ID INT NOT NULL,
-Student_ID INT NOT NULL,
-Faculty_ID INT NOT NULL,
-Course_ID INT NOT NULL,
+Student_ID INT,
+Faculty_ID INT,
+Course_ID VARCHAR(10) NOT NULL,
 Position_Type VARCHAR(10) NOT NULL,
 PRIMARY KEY (Position_ID),
-FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Faculty_ID) references Faculty (Faculty_ID) ON DELETE RESTRICT,
-FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE RESTRICT);
+FOREIGN KEY (Student_ID) references Student (Student_ID) ON DELETE SET NULL,
+FOREIGN KEY (Faculty_ID) references Faculty (Faculty_ID) ON DELETE SET NULL,
+FOREIGN KEY (Course_ID) references Course (Course_ID) ON DELETE CASCADE);
